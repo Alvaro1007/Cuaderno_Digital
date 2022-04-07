9ab4d3dc536add8531eb2a30e18e0e067d62802f
Alvaro1007 <101581778+Alvaro1007@users.noreply.github.com>
2022-04-07 16:22:35 +0200

    Add files via upload

--- matriculas.c
+++ matriculas.c
@@ -70,27 +70,97 @@ void listarMatriculas(Matricula *data, int arraySize){
 	}
 }
 
+
 void imprimeMatricula(Matricula m){
-	printf("\n -Id_Materia: %.4d\n -Id_Alumno: %.6d\n",m.id_mat,m.id_alum);
+   printf("\n -Materia:%.4d\n -Alumno: %.6d\n",m.id_mat, m.id_alum);
 }
 
 
+void listaMateriasAlumno(Matricula *data, int arraySize, int id_alum){
+	int i;
+	
+	for(i=0; i<arraySize;i++){
+		if(id_alum==data[i].id_alum){
+			printf("MATRICULA %d\n",i+1);
+			imprimeMatricula(data[i]);
+			printf("\n");
+		}
+	}	
+}
+
+void addMatricula(Matricula** matriculas, int* nMatriculas, int id_alum){
+	int i;
+	
+	printf("A%cADIR MATRICULA\n\n", 165);
+	
+	if(((*matriculas) = realloc((*matriculas), (*nMatriculas+1) * sizeof(Matricula))) == NULL){
+			printf("Error, no hay suficiente memoria disponible.\n");
+	}
+	
+	i = (*nMatriculas);
+		
+	*nMatriculas=*nMatriculas + 1;
+	(*matriculas)[i].id_alum = id_alum;
+	
+	cambiarMateria(i, &*matriculas);
+	
+	
+	
+	listaMateriasAlumno(&**matriculas, *nMatriculas, id_alum);
+}
+
+void modificarMatricula(Matricula** data, int nMatriculas, int id_alum){
+	
+	int i, j, aux, encontrado=0, cont=1; 
+	
+	printf("MODIFICAR MATRICULA\n\n");
+	//listamos las matriculas de un alumno 
+	listaMateriasAlumno(&**data, nMatriculas, id_alum);
+	//seleccionamos la matricula que deseamos modificar mediante un menu basico
+	printf("Que matricula desea modificar?: ");
+	scanf("%d",&aux);
+	fflush(stdin);
+	
+	cambiarMateria(aux-1, &*data);
+}
+
+void eliminarMatricula(Matricula** data, int* nMatriculas, int id_alum){
+	int i, aux; 
+	printf("ELIMINAR CALIFICACIONES\n\n");
+	
+	listaMateriasAlumno(&**data, *nMatriculas, id_alum);
+
+	printf("Que matricula desea eliminar?: ");
+	scanf("%d",&aux);
+	fflush(stdin);
+	
+	imprimeMatricula((*data)[aux-1]);
+	
+	printf("\nDesea eliminar la matricula %d?(Escriba '1' si asi lo desea): ",aux);
+	scanf("%d",&i);
+	fflush(stdin);
+	if (i!=1) printf("Operacion cancelada, volviendo al men%c\n", 163);
+	else{			
+	
+		((*data)[aux-1].id_mat) = ((*data)[(*nMatriculas-1)].id_mat);
+		((*data)[aux-1].id_alum) = ((*data)[(*nMatriculas-1)].id_alum);
+		
+	// Reasignamos la memoria reservada y actualizamos el numero de materias
+		(*data) = realloc((*data), (*nMatriculas-1) * sizeof(Matricula));
+		(*nMatriculas)=(*nMatriculas-1);
+	}
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+void cambiarMateria(int id, Matricula** data){
+	int materia;
+	
+	printf("Introduce la nueva materia: ");
+	scanf("%d",&materia);
+	
+	(*data)[id].id_mat = materia;
+	printf("\n");	
+}
 
 
 
--- matriculas.h
+++ matriculas.h
@@ -12,5 +12,11 @@ void setMatriculas(Matricula* data, int i);
 
 void listarMatriculas(Matricula *data, int arraySize);
 void imprimeMatricula(Matricula m);
+void listaMateriasAlumno(Matricula *data, int arraySize, int id_alum);
+
+void addMatricula(Matricula** matriculas, int* nMatriculas, int id_alum);
+void modificarMatricula(Matricula** data, int nMatriculas, int id_alum);
+void eliminarMatricula(Matricula** data, int* nMatriculas, int id_alum);
+void cambiarMateria(int id, Matricula** data);
 
 #endif
